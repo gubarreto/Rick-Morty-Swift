de430600ff146d944e2963935f73f6b0e74121e2
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-10-09 15:44:21 -0300

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Gustavo on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
